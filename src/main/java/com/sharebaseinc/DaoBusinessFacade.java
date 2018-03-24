6550cc22774afda75137b93054b0b08bd3d9876c
kouichi <kouichiume@gmail.com>
2018-03-24 23:16:53 +0900

    spring-web3jから呼び出し まだ web3jがないって言われる

---
+++ src/main/java/com/sharebaseinc/DaoBusinessFacade.java
@@ -0,0 +1,20 @@
+package com.sharebaseinc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.web3j.protocol.Web3j;
+
+@Service
+public class DaoBusinessFacade {
+
+	@Autowired
+	private Web3j web3j;
+	
+
+	
+	
+	
+	
+	
+
+}
