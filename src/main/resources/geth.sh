0738e68b92b3f9fa49cdd2a4f75afc0a27af56f5
kouichi <kouichiume@gmail.com>
2018-03-29 01:35:33 +0900

    whisper 起動を下に追加

--- src/main/resources/geth.sh
+++ src/main/resources/geth.sh
@@ -47,3 +47,10 @@ printf '%d\n' "0x6f05b59d3b20000"
  nohup geth --networkid 4649 --nodiscover --maxpeers 0 --datadir ~/data_testnet --mine --minerthreads 1 --rpc --rpcaddr "0.0.0.0" --rpcport 8545  --rpccorsdomain "*" --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" --unlock 0,1 --password ~/data_testnet/password --verbosity 6 2>> ~/data_testnet/geth.log &
 
 geth attach rpc:http://localhost:8545
+
+
+
+
+
+#wisper
+geth --shh --testnet --nodiscover console
