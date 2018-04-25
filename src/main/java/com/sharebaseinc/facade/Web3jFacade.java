d24b181ae5ae0b57a4f2a7ab794d2f39e43ca7ce
kouichi <kouichiume@gmail.com>
2018-04-26 00:34:55 +0900

    java resceive result of eth transaction

--- src/main/java/com/sharebaseinc/facade/Web3jFacade.java
+++ src/main/java/com/sharebaseinc/facade/Web3jFacade.java
@@ -3,13 +3,16 @@ package com.sharebaseinc.facade;
 import java.io.IOException;
 import java.math.BigInteger;
 
-import javax.swing.table.DefaultTableColumnModel;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.Transaction;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+
+import rx.Observable;
+import rx.Observer;
+import rx.schedulers.Schedulers;
 
 @Service
 public class Web3jFacade {
@@ -34,6 +37,33 @@ public class Web3jFacade {
 		DefaultBlockParameter defaultBlockParameter = null;
 
 		web3j.ethGetStorageAt(address, position, defaultBlockParameter);
+
+	}
+
+	public void fileText() {
+
+		Observable<Transaction> ob = web3j.transactionObservable();
+		ob.observeOn(Schedulers.computation()).subscribe(new Observer<Transaction>() {
+
+			@Override
+			public void onCompleted() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onError(Throwable e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void onNext(Transaction t) {
+				// TODO Auto-generated method stub
+				
+			}
+
+		});
 
 	}
 
